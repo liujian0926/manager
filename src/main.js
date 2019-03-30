9175c43a70bd9eac6803e6c219c978257578b828
liujian <329751289@qq.com>
2019-03-30 10:17:36 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
+// 导入顶级vue
 import Vue from 'vue'
+// 导入app.vue
 import App from './App.vue'
-
+// 导入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+// 定义路由规则
+const routes = [
+  
+]
+// 创建路由对象
+const router = new VueRouter({
+  routes 
+})
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el:'#app',
+  render: h => h(App)
+})
