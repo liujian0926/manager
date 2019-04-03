d08bd4c2c9e4dd420234255ab751a704fc86052a
liujian <329751289@qq.com>
2019-04-03 16:34:09 +0800

    完成权限列表渲染

--- src/components/error.vue
+++ src/components/error.vue
@@ -17,6 +17,7 @@ export default {
   .error {
      img {
       display: block;
+      height: 100%;
       width: 100%;
       overflow: hidden;
      }
--- src/components/rights.vue
+++ src/components/rights.vue
@@ -5,10 +5,16 @@
 
     <!-- 表格 -->
     <el-table :data="rightsList"  style="width: 100%" border>
-      <el-table-column type="index" width="50"></el-table-column>
-      <el-table-column  label="权限名称" width="180"></el-table-column>
-      <el-table-column label="路径" width="180"></el-table-column>
-      <el-table-column label="层级" width="180"></el-table-column>
+      <el-table-column  type="index" width="50"></el-table-column>
+      <el-table-column  prop="authName" label="权限名称" width="180">一级</el-table-column>
+      <el-table-column prop="path" label="路径" width="180"></el-table-column>
+      <el-table-column prop="level" label="层级" width="180">
+        <template slot-scope="scope">
+          <span v-if="scope.row.level==0">一级</span>
+          <span v-else-if="scope.row.level==1">二级</span>
+          <span v-else>三级</span>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -20,6 +26,14 @@ export default {
     return {
       rightsList:[{},{}]
     }
+  },
+
+ async created () {
+   //  所以权列表渲染
+    let res =await this.$axios.get('rights/list')
+    console.log(res);
+    this.rightsList = res.data.data
+    
   }
 };
 </script>
